2e984022303f97d41d527f87ea242d503dfed3a5
Kevin L <kevinliu20210000@gmail.com>
2025-12-08 19:56:13 -0600

    update server js for vercel

--- server/server.js
+++ server/server.js
@@ -67,7 +67,13 @@ app.use((err, req, res, next) => {
 //connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000
-server.listen(PORT, () => {
-    console.log('server is runnning on PORT : ' + PORT)
-})
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000
+    server.listen(PORT, () => {
+        console.log('server is runnning on PORT : ' + PORT)
+    })
+}
+
+//export server for vercel
+export default server;
+
